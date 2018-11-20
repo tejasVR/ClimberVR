c29be0f38bca257f0c8441f6e54e8a16c663f305
Tejas Shroff <tejasshroff24@gmail.com>
2018-11-20 16:40:00 -0500

    First pass at working with SteamVR v2 API

---
+++ Assets/App/Scripts/Interactable Objects/Climbable.cs
@@ -0,0 +1,52 @@
+﻿//======= Copyright (c) Aperion Studios, All rights reserved. ===============
+//
+// Purpose: Creates a basic climable objects for players to climb 
+//
+//=============================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace AperionStudios.ClimberVR
+{
+    [RequireComponent(typeof(Interactable))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class Climbable : MonoBehaviour
+    {
+        [HideInInspector]
+        public Interactable interactable;
+
+        protected void Awake()
+        {
+            interactable = GetComponent<Interactable>();
+        }
+
+        protected virtual void OnAttachedToHand(Hand hand)
+        {
+            Debug.Log("A hand interacted with a climable object");
+        }
+
+        protected virtual void OnHandHoverBegin(Hand hand)
+        {
+            //Debug.Log("A hand hovers over a with a climable object");
+            if (hand.GetComponent<PlayerClimb>() != null)
+            {               
+                hand.GetComponent<PlayerClimb>().SetCanClimb(true);               
+            }            
+        }
+
+        protected virtual void OnHandHoverEnd(Hand hand)
+        {
+            //Debug.Log("A hand hovers over a with a climable object");
+            if (hand.GetComponent<PlayerClimb>() != null)
+            {
+                hand.GetComponent<PlayerClimb>().SetCanClimb(false);
+            }
+        }
+    }
+}
+
+
